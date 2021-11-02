1575ca9acd0a299c299fc96e2a7c26b88df4c060
Matthew Nicholas <mattnick927@gmail.com>
2021-11-02 11:40:23 +0100

    Updated register page's front end

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,6 +1,33 @@
 import React from "react";
 
+import { useState } from "react";
+import ReactDOM from "react-dom";
+
 function Register() {
+  const [inputs, setInputs] = useState('');
+
+  const handleChange = (e) => {
+    alert(inputs);
+    const varr = {inputs};
+    console.log(varr);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    
+    
+
+    // fetch('http://localhost:3000/blogs/', {
+    //   method: 'POST',
+    //   headers: { "Content-Type": "application/json" },
+    //   body: JSON.stringify(blog)
+    // }).then(() => {
+    //   console.log('new blog added');
+    // })
+  }
+
   return (
     <div className="register">
       <div class="container">
@@ -17,19 +44,28 @@ function Register() {
               </div>
             
             
-            <p></p>
-            <p></p>
-            <p></p>
+            <p>{ inputs }</p>
 
             <form>
               
               <div class="row">
                   <div class="col-lg-4">
                     <label>
-                      Name:
+                      First Name:
                     </label>
                   </div>
-                  <input class="col-lg-8" type="text" name="name"/>
+                  <input class="col-lg-8" type="text" name="f_name" value={inputs} onChange={(e) => setInputs(e.target.value)}/>
+              </div>
+
+              <p></p>
+
+              <div class="row">
+                  <div class="col-lg-4">
+                    <label>
+                      Last Name:
+                    </label>
+                  </div>
+                  <input class="col-lg-8" type="text" name="l_name" onChange={handleChange}/>
               </div>
 
               <p></p>
@@ -40,7 +76,73 @@ function Register() {
                       Email:
                     </label>
                   </div>
-                  <input class="col-lg-8" type="text" name="name"/>
+                  <input class="col-lg-8" type="text" name="email"/>
+              </div>
+
+              <p></p>
+
+              <div class="row">
+                  <div class="col-lg-4">
+                    <label>
+                      Password:
+                    </label>
+                  </div>
+                  <input class="col-lg-8" type="password" name="password"/>
+              </div>
+
+              <p></p>
+
+              <div class="row">
+                  <div class="col-lg-4">
+                    <label>
+                      Confirm Password:
+                    </label>
+                  </div>
+                  <input class="col-lg-8" type="password" name="c_password"/>
+              </div>
+
+              <p></p>
+
+              <div class="row">
+                  <div class="col-lg-4">
+                    <label>
+                      Street Name:
+                    </label>
+                  </div>
+                  <input class="col-lg-8" type="text" name="street_name"/>
+              </div>
+
+              <p></p>
+
+              <div class="row">
+                  <div class="col-lg-4">
+                    <label>
+                      State:
+                    </label>
+                  </div>
+                  <input class="col-lg-8" type="text" name="state"/>
+              </div>
+
+              <p></p>
+
+              <div class="row">
+                  <div class="col-lg-4">
+                    <label>
+                      Postal Code:
+                    </label>
+                  </div>
+                  <input class="col-lg-8" type="text" name="post"/>
+              </div>
+
+              <p></p>
+
+              <div class="row">
+                  <div class="col-lg-4">
+                    <label>
+                      Country:
+                    </label>
+                  </div>
+                  <input class="col-lg-8" type="text" name="country"/>
               </div>
 
               <p></p>
@@ -51,7 +153,7 @@ function Register() {
                       Phone Number:
                     </label>
                   </div>
-                  <input class="col-lg-8" type="number" name="name"/>
+                  <input class="col-lg-8" type="number" name="phone"/>
               </div>
 
               <p></p>
@@ -73,5 +175,7 @@ function Register() {
     </div>
   );
 }
+
+ReactDOM.render(<Register />, document.getElementById('root'));
 
 export default Register;
