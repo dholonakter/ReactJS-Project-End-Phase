180282c8730610a163bbf2dd905f0c0dc8b7caec
Matthew Nicholas <mattnick927@gmail.com>
2021-11-20 00:14:41 +0100

    Register Page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,32 +1,76 @@
 import React from "react";
-
+import axios from 'axios';
+//import {Button} from '@material-ui/core';
+import Buttons from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import TextFields from '@material-ui/core/TextField';
 import { useState } from "react";
-import ReactDOM from "react-dom";
+//import ReactDOM from "react-dom";
 
 function Register() {
-  const [inputs, setInputs] = useState('');
+  const [f_name, setF_name] = useState('');
+  const [l_name, setL_name] = useState('');
+  const [phone, setPhone] = useState('');
 
-  const handleChange = (e) => {
-    alert(inputs);
-    const varr = {inputs};
-    console.log(varr);
-  }
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [c_password, setCPassword] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // const blog = { title, body, author };
+  const [street, setStreet] = useState('');
+  const [statee, setStatee] = useState('');
+  const [post, setPost] = useState('');
+  const [country, setCountry] = useState('');
+  const [arr, setArr]= useState('');
 
+
+  const handleSubmit = event => {
+    //event.preventDefault();
+    let formData = new FormData();
+    setArr({"f_name":f_name,"l_name":l_name,"phone":phone,"email":email,"password":password,"street":street,"statee":statee,"post":post,"country":country});
+    formData.append('f_name', f_name);
+    formData.append('l_name',l_name);
+    formData.append('phone',phone);
+    formData.append('email',email);
+    formData.append('password',password);
+    formData.append('street',street);
+    formData.append('statee',statee);
+    formData.append('post',post);
+    formData.append('country',country);
+
+    var valid = 1;
+    if(email.includes('@',1) && email.includes('.',2)){}else{
+        valid = 0;
+        alert("Email address is invalid!");
+    }
+      
+      if(password.length >= 8 && (new RegExp(/^((([0-9])+([a-z]))|(([a-z])+([0-9])))/, 'i')).test(password)){
+
+        if(password != c_password){
+          valid = 0;
+          alert("Password and confirm password needs to be the same!");
+        }
+      }else{
+        valid = 0;
+        alert("Password needs to be at least 8 characters long and contain both letters and numbers");
+      }
+
+      if(valid == 1){
+        axios({
+          method: 'POST',
+          url:'https://i383988.hera.fhict.nl/registerUser/registerUser.php?',
+          data: formData,
+          config: {headers:{'Content-Type': 'multipart/form-data'}}
+        }).then(function(response){
+          console.log(response);
+          console.log('New User Added');
+          alert("Registration Completed!");
+        })
+      }
+      else{
+        alert("Please enter valid data!");
+      }
+    }
     
-    
-
-    // fetch('http://localhost:3000/blogs/', {
-    //   method: 'POST',
-    //   headers: { "Content-Type": "application/json" },
-    //   body: JSON.stringify(blog)
-    // }).then(() => {
-    //   console.log('new blog added');
-    // })
-  }
 
   return (
     <div className="register">
@@ -39,121 +83,185 @@ function Register() {
             
               <div className="row">
                 
-                <div className="col-lg-4"></div>
-                <h1 className="font-weight-light col-lg-4"><center>Register</center></h1>
+              <div className="col-lg-10"><center>
+                <Typography className="font-weight-light" variant="h2">Register</Typography></center>
+                </div>
               </div>
             
             
-            <p>{ inputs }</p>
+            <p></p>
 
+            {/* <form onSubmit={handleSubmit}> */}
             <form>
+
+              <div className="row">
+                <Typography className="font-weight-light col-lg-12" variant="h4">Personal Information</Typography>
+                {/* <h2 className="font-weight-light col-lg-12">Personal Information</h2> */}
+              </div>
+
+              <p></p>
               
               <div className="row">
-                  <div className="col-lg-4">
-                    <label>
+                  {/* <div className="col-lg-4" variant="h5">
+                    <Typography>
                       First Name:
-                    </label>
-                  </div>
-                  <input className="col-lg-8" type="text" name="f_name" value={inputs} onChange={(e) => setInputs(e.target.value)}/>
+                    </Typography>
+                  </div> */}
+                  <div className="col-lg-1"></div>
+                  <TextFields required
+                  className="col-lg-11" type="text" name="f_name" label="First Name" value={f_name} variant="outlined"
+                  onChange={(e) => setF_name(e.target.value)}
+                  />
+                  {/* <input className="col-lg-8" type="text" name="f_name" value={f_name} onChange={(e) => setF_name(e.target.value)}/> */}
               </div>
 
               <p></p>
 
               <div className="row">
-                  <div className="col-lg-4">
-                    <label>
+                  {/* <div className="col-lg-4">
+                    <Typography>
                       Last Name:
-                    </label>
+                    </Typography>
                   </div>
-                  <input className="col-lg-8" type="text" name="l_name" onChange={handleChange}/>
+                  <input className="col-lg-8" type="text" name="l_name" value={l_name} onChange={(e) => setL_name(e.target.value)}/> */}
+                  <div className="col-lg-1"></div>
+                  <TextFields required
+                  className="col-lg-11" variant="outlined" label="Last Name" type="text" name="l_name" value={l_name} onChange={(e) => setL_name(e.target.value)}
+                  />
               </div>
 
               <p></p>
-
+              
               <div className="row">
-                  <div className="col-lg-4">
-                    <label>
-                      Email:
-                    </label>
-                  </div>
-                  <input className="col-lg-8" type="text" name="email"/>
-              </div>
-
-              <p></p>
-
-              <div className="row">
-                  <div className="col-lg-4">
-                    <label>
-                      Password:
-                    </label>
-                  </div>
-                  <input className="col-lg-8" type="password" name="password"/>
-              </div>
-
-              <p></p>
-
-              <div className="row">
-                  <div className="col-lg-4">
-                    <label>
-                      Confirm Password:
-                    </label>
-                  </div>
-                  <input className="col-lg-8" type="password" name="c_password"/>
-              </div>
-
-              <p></p>
-
-              <div className="row">
-                  <div className="col-lg-4">
-                    <label>
-                      Street Name:
-                    </label>
-                  </div>
-                  <input className="col-lg-8" type="text" name="street_name"/>
-              </div>
-
-              <p></p>
-
-              <div className="row">
-                  <div className="col-lg-4">
-                    <label>
-                      State:
-                    </label>
-                  </div>
-                  <input className="col-lg-8" type="text" name="state"/>
-              </div>
-
-              <p></p>
-
-              <div className="row">
-                  <div className="col-lg-4">
-                    <label>
-                      Postal Code:
-                    </label>
-                  </div>
-                  <input className="col-lg-8" type="text" name="post"/>
-              </div>
-
-              <p></p>
-
-              <div className="row">
-                  <div className="col-lg-4">
-                    <label>
-                      Country:
-                    </label>
-                  </div>
-                  <input className="col-lg-8" type="text" name="country"/>
-              </div>
-
-              <p></p>
-
-              <div className="row">
-                  <div className="col-lg-4">
-                    <label>
+                  {/* <div className="col-lg-4">
+                    <Typography>
                       Phone Number:
-                    </label>
+                    </Typography>
                   </div>
-                  <input className="col-lg-8" type="number" name="phone"/>
+                  <input className="col-lg-8" type="number" name="phone" value={phone} onChange={(e) => setPhone(e.target.value)}/> */}
+                  <div className="col-lg-1"></div>
+                  <TextFields required
+                  className="col-lg-11" variant="outlined" label="Phone" type="number" name="phone" value={phone} onChange={(e) => setPhone(e.target.value)}
+                  />
+              </div>
+
+              <p></p>
+
+              <div className="row">
+                <Typography className="font-weight-light col-lg-12" variant="h4">Account Information</Typography>
+                {/* <h2 className="font-weight-light col-lg-12">Account Information</h2> */}
+              </div>
+              
+              <p></p>
+
+              <div className="row">
+                  {/* <div className="col-lg-4">
+                    <Typography>
+                      Email:
+                    </Typography>
+                  </div>
+                  <input className="col-lg-8" type="text" name="email" value={email} onChange={(e) => setEmail(e.target.value)}/> */}
+                  <div className="col-lg-1"></div>
+                  <TextFields required
+                  className="col-lg-11" variant="outlined" label="Email" type="text" name="email" value={email} onChange={(e) => setEmail(e.target.value)}
+                  />
+              </div>
+
+              <p></p>
+
+              <div className="row">
+                  {/* <div className="col-lg-4">
+                    <Typography>
+                      Password:
+                    </Typography>
+                  </div>
+                  <input className="col-lg-8" type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}/> */}
+                  <div className="col-lg-1"></div>
+                  <TextFields required
+                  className="col-lg-11" variant="outlined" label="Password" type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}
+                  />
+              </div>
+
+              <p></p>
+
+              <div className="row">
+                  {/* <div className="col-lg-4">
+                    <Typography>
+                      Confirm Password:
+                    </Typography>
+                  </div>
+                  <input className="col-lg-8" type="password" name="c_password" value={c_password} onChange={(e) => setCPassword(e.target.value)}/> */}
+                  <div className="col-lg-1"></div>
+                  <TextFields required
+                  className="col-lg-11" variant="outlined" label="Confirm Password" type="password" name="c_password" value={c_password} onChange={(e) => setCPassword(e.target.value)}
+                  />
+              </div>
+
+              <p></p>
+
+              <div className="row">
+                <Typography className="font-weight-light col-lg-12" variant="h4">Address</Typography>
+                {/* <h2 className="font-weight-light col-lg-12">Address</h2> */}
+              </div>
+
+              <p></p>
+
+              <div className="row">
+                  {/* <div className="col-lg-4">
+                    <Typography>
+                      Street Name:
+                    </Typography>
+                  </div>
+                  <input className="col-lg-8" type="text" name="street_name" value={street} onChange={(e) => setStreet(e.target.value)}/> */}
+                  <div className="col-lg-1"></div>
+                  <TextFields required
+                  className="col-lg-11" variant="outlined" label="Street Name" type="text" name="street_name" value={street} onChange={(e) => setStreet(e.target.value)}
+                  />
+              </div>
+
+              <p></p>
+
+              <div className="row">
+                  {/* <div className="col-lg-4">
+                    <Typography>
+                      State:
+                    </Typography>
+                  </div>
+                  <input className="col-lg-8" type="text" name="statee" value={statee} onChange={(e) => setStatee(e.target.value)}/> */}
+                  <div className="col-lg-1"></div>
+                  <TextFields required
+                  className="col-lg-11" variant="outlined" label="State" type="text" name="statee" value={statee} onChange={(e) => setStatee(e.target.value)}
+                  />
+              </div>
+
+              <p></p>
+
+              <div className="row">
+                  {/* <div className="col-lg-4">
+                    <Typography>
+                      Postal Code:
+                    </Typography>
+                  </div>
+                  <input className="col-lg-8" type="text" name="post" value={post} onChange={(e) => setPost(e.target.value)}/> */}
+                  <div className="col-lg-1"></div>
+                  <TextFields required
+                  className="col-lg-11" variant="outlined" label="Post COde" type="text" name="post" value={post} onChange={(e) => setPost(e.target.value)}
+                  />
+              </div>
+
+              <p></p>
+
+              <div className="row">
+                  {/* <div className="col-lg-4">
+                    <Typography>
+                      Country:
+                    </Typography>
+                  </div>
+                  <input className="col-lg-8" type="text" name="country" value={country} onChange={(e) => setCountry(e.target.value)}/> */}
+                  <div className="col-lg-1"></div>
+                  <TextFields required
+                  className="col-lg-11" variant="outlined" label="Country" type="text" name="country" value={country} onChange={(e) => setCountry(e.target.value)}
+                  />
               </div>
 
               <p></p>
@@ -162,8 +270,8 @@ function Register() {
                   <div className="col-lg-10">
                     
                   </div>
-                  <input className="col-lg-2" type="submit" value="Submit" />
-              
+                  {/* <button className="button col-lg-2 btn-primary btn-block" onClick={handleSubmit}>button</button> */}
+                  <Buttons className="col-lg-2" variant="contained" color="primary" onClick={handleSubmit}>Register</Buttons>
               </div>
 
             </form>
