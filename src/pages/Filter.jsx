ca65d90bc396390f0d0be86638d1e491fc7d834b
Ryan FM <ryanmeijs@hotmail.com>
2021-11-22 03:29:01 +0100

    Get data from db and completed filtering

--- src/pages/Filter.jsx
+++ src/pages/Filter.jsx
@@ -1,100 +1,245 @@
 import React from "react";
-import ReactDOM from 'react-dom'
-import { FormGroup, FormControlLabel, Checkbox, Slider, Stack, TablePagination} from '@mui/material';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import {Typography, TextField, Button, Slider, Checkbox, FormGroup, FormControlLabel} from '@material-ui/core';
 
 import Navigation from "../components/Navigation"
 import Footer from "../components/Footer"
-import ProductFilter from "../components/ProductFilter"
 
 class Filter extends React.Component {
 	
-	
-	constructor() {
-    super();
-    this.state = {
-      todos: ['a','b','c','d','e','f','g','h','i','j','k'],
-      currentPage: 1,
-      todosPerPage: 3
-    };
-    this.handleClick = this.handleClick.bind(this);
+  constructor(props){
+	  super(props);
+	  this.state = { 
+	    products: [], 
+		filteredProducts: [],
+		nameValue: "",
+		categories: [ // number of categories = number of values
+		{
+		  id: 1,
+		  label: "Books",
+		},
+        {
+		  id: 2,
+		  label: "Accessories",
+		},		
+		],
+		bookSelected: true,
+		accSelected: true,
+		priceValue: 100,
+		sliderValue: 500,
+		marks:[
+		{
+			value: 0,
+			label: "€0",
+		},
+		{
+			value: 500,
+			label: "€100",
+		},
+		{
+			value: 1000,
+			label: "€1000",
+		},
+		]
+	  };
+	  // Fixes undefined error when setting the state
+	  this.handleFilter = this.handleFilter.bind(this);
+	  this.handleReset = this.handleReset.bind(this);
+	  this.handleSlideChange = this.handleSlideChange.bind(this);
+	  this.handleCheckChange = this.handleCheckChange.bind(this);
+	  this.handleTextChange = this.handleTextChange.bind(this);
   }
-
-  handleClick(event) {
-    this.setState({
-      currentPage: Number(event.target.id)
-    });
+	
+  componentDidMount(){
+	const url = 'https://i383988.hera.fhict.nl/searchProduct/searchProduct.php?';
+	axios.get(url).then(response => response.data)
+	.then((data) => {
+		this.setState({ products: data});
+		this.setState({ filteredProducts: data});
+	})
   }
-	
-	
-	render(){
-		const { todos, currentPage, todosPerPage } = this.state;
-
-    // Logic for displaying todos
-    const indexOfLastTodo = currentPage * todosPerPage;
-    const indexOfFirstTodo = indexOfLastTodo - todosPerPage;
-    const currentTodos = todos.slice(indexOfFirstTodo, indexOfLastTodo);
-
-    const renderTodos = currentTodos.map((todo, index) => {
-      return <li key={index}>{todo}</li>;
-    });
-
-    // Logic for displaying page numbers
-    const pageNumbers = [];
-    for (let i = 1; i <= Math.ceil(todos.length / todosPerPage); i++) {
-      pageNumbers.push(i);
-    }
-
-    const renderPageNumbers = pageNumbers.map(number => {
-      return (
-        <li
-          key={number}
-          id={number}
-          onClick={this.handleClick}
-        >
-          {number}
-        </li>
-      );
-    });
-	
-	
-	let productFilter = new ProductFilter();
-	let productList = productFilter.getList();
-	let filteredList = productFilter.sortList("PRICE", 40);
-
-  return (
-    <div className="filter">
-	  <div>
-        <Navigation/>    
-      </div>
-	  <div className="col-lg-7">
-            <img
-              className="img-fluid rounded mb-4 mb-lg-0"
-              //src="http://placehold.it/900x400"
-            />
-          </div>
-	  <div>
-        <ul>
-          {renderTodos}
-        </ul>
-        <ul id="page-numbers">
-          {renderPageNumbers}
-        </ul>
-      </div>
-
-      <div className="container">  
-      <FormGroup>
-        <FormControlLabel control={<Checkbox/>} label="Books" />
-		<FormControlLabel control={<Checkbox/>} label="Accessories" />
-		<FormControlLabel control={<Checkbox/>} label="Other" />
+  
+   
+  filterList(){
+	    let productList = this.state.products;
+		let filteredList = [];
+		let nameValue = this.state.nameValue;
+		let categories = this.state.categories;
+		let priceValue = this.state.priceValue;
 		
-      </FormGroup> 
-      <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
-        <Slider aria-label="Price" />
-      </Stack>
-			     
-        </div>
-		<Footer id="footer"/>     
-    </div>	
+		for( let product of productList) {
+		  let priceCheck = false;
+		  let catCheck = false;
+		  let nameCheck = false;
+		  
+		  for(let cat of categories){
+			if(product.category_id == 1 && this.state.bookSelected){
+			  catCheck = true;  
+		    }else if(product.category_id == 2 && this.state.accSelected){
+			  catCheck = true;
+			}
+		  }
+		  if(product.product_description == nameValue || nameValue == ""){
+				nameCheck = true;
+			}
+		  if(product.product_price <= priceValue){
+				priceCheck = true;
+			}
+          if(priceCheck && catCheck && nameCheck){
+			filteredList.push(product);
+		  }	
+		}
+		return filteredList;
+		
+	}
+	
+  handleTextChange(event){
+	let textValue = event.target.value;
+    this.setState({ nameValue: textValue });
+	console.log(textValue);
+  }	
+	
+  handleCheckChange(event){
+    switch(event.target.name){
+	  case "Books":
+	    this.setState({ bookSelected: event.target.checked });
+	    break;
+	  case "Accessories": 
+	    this.setState({ accSelected: event.target.checked })
+	    break;
+	}
+  }
+	
+  handleSlideChange(event, value){
+	let scaledValue = this.calculateValue(value);
+	this.setState({ priceValue: scaledValue });
+	this.setState({ sliderValue: value });
+  }
+  
+  valueLabelFormat(value){ // Not using the calculateValue function here - It causes an error as it doesn't get defined properly.
+	let newValue = value; 
+	if(value < 500){
+		newValue = value / 5;
+	}else{
+		newValue = ((value - 500) / 5 * 9) + 100;
+		
+	}
+	  let valueText = `€${newValue}`;
+	  
+	  return valueText;
+  }
+  
+  calculateValue(value){
+	let newValue = value; 
+	if(value < 500){
+		newValue = value / 5;
+	}else{
+		newValue = ((value - 500) / 5 * 9) + 100;
+		
+	}
+	return newValue
+  }
+  
+  getCategoryName(value){
+	let catName;
+	for (let cat of this.state.categories){
+	  if (value == cat.id){
+		catName = cat.label;
+	  }
+	}
+	return catName;
+  }
+	
+  handleFilter(){
+	  this.setState({ filteredProducts: this.filterList()});
+  }
+  
+  handleReset(){
+	this.setState({ filteredProducts: this.state.products});
+  }
+	
+  render(){	
+  
+  return (
+    <div className="filter">	  
+      <div className="container">
+	    <div className="col-xs-8">
+		  <h1> Search products </h1>
+		  <div className="prod-search">
+            <div className="prod-search-result">
+		      <table className="table table-striped">
+		      <thead className="thead-light">
+			    <tr>
+			    <th>Name</th>
+			    <th>Price</th>
+			    <th>Category</th>
+				</tr>
+			  </thead>
+			  <tbody>
+			  {this.state.filteredProducts.map((pr, index) => (
+			    <tr key={index}>
+			      <td>{pr.product_description}</td>
+			      <td>{pr.product_price}</td>
+			  	  <td>{this.getCategoryName(pr.category_id)}</td>
+			    </tr>
+			  ))}
+			  </tbody>
+		    </table>
+            </div>
+            <div className="prod-search-filter">
+			  <div className="prod-search-name">
+			    <TextField
+				  label="Name"
+				  id="name-search"
+				  size="small"
+				  value={this.state.nameValue}
+				  onChange={this.handleTextChange}
+				/>
+			  </div>
+			  <div className="prod-search-price">
+			    <Typography>Price</Typography>
+			    <Slider
+				  value={this.state.sliderValue}
+				  defaultValue={100}
+				  aria-label="Price slider"
+				  getAriaValueText={this.valueLabelFormat}
+				  valueLabelFormat={this.valueLabelFormat}
+				  step={20}
+				  min={0}
+                  max={1000}
+				  marks={this.state.marks}
+				  onChange={this.handleSlideChange}
+				  valueLabelDisplay="auto"
+				/>				
+			  </div>
+			  <div className="prod-search-cat">
+			    <FormGroup>
+				  <FormControlLabel 
+				    control={
+					  <Checkbox checked={this.state.bookSelected} onChange={this.handleCheckChange} name="Books"/>
+				    } label="Books" />
+				  <FormControlLabel 
+				    control={
+					  <Checkbox checked={this.state.accSelected} onChange={this.handleCheckChange} name="Accessories"/>
+				    } label="Accessories" />
+				</FormGroup>
+			  </div>
+			  <div>
+			  <Button className="col-lg-2" variant="contained" color="primary" onClick={this.handleFilter}>Search</Button>
+			  <Button className="col-lg-2" variant="contained" color="primary" onClick={this.handleReset}>Reset</Button>
+			  </div>
+            </div>
+          </div>
+          <div className="prod-search-bottom">
+		  </div>		  
+		</div>
+		
+      
+	  
+      <Footer />     
+      </div>
+    </div>	  
   );
 	}
 }
