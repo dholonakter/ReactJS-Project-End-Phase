023ed6fa729161fa5727dfffb7cf718300a214bc
RyanFM <polyphynx@gmail.com>
2022-01-10 15:36:10 +0100

    Add product form validation

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -18,6 +18,7 @@ class AddProduct extends React.Component {
 	    productName: "",
 		productDesc: "",
 		price: null,
+		parsedPrice: null,
 		category: "",
 		imgName: "",
 		productImg: null,
@@ -40,7 +41,24 @@ class AddProduct extends React.Component {
   }
     handlePriceChange(event){
 	let priceValue = event.target.value;
-    this.setState({ price: priceValue });
+	let parsedValue = parseFloat(priceValue).toFixed(2);
+	
+	console.log(priceValue);
+	console.log(parsedValue);
+	
+	let numStr = String(priceValue);
+	let decimalAmt = 0;
+   // String Contains Decimal
+    if (numStr.includes('.')) {
+      decimalAmt = numStr.split('.')[1].length;
+    }
+	
+	if(decimalAmt > 2){		
+      this.setState({ price: parsedValue });
+	}else{
+	  this.setState({ price: priceValue });
+	}	
+	this.setState({ parsedPrice: parsedValue });
   }
     handleCategoryChange(event){
     let catValue = event.target.value;
@@ -58,6 +76,9 @@ class AddProduct extends React.Component {
 			  break;
 			case "bicycle": 
 			  categoryId = 3
+			  break;
+			case "other":
+			  categoryId = 4
 			  break;
 		}
 	  return categoryId;
@@ -79,18 +100,12 @@ class AddProduct extends React.Component {
 	}
   
     handleSubmit(event){
-      let formData = new FormData();
-	  let categoryId = this.getCategoryId(this.state.category);
-	  let imageFile = this.state.productImg;
-	  console.log(imageFile);
-      let valid = 1;
-      if(categoryId != -1 && this.state.productName != "" && this.state.productDesc != "" && this.state.price != null && imageFile != null && this.state.user.value.id != null){}else{
-        valid = 0;
-        alert("Something went wrong!");
-      }
-	  
+	  if(this.validateForm()){
+        let formData = new FormData();
+	    let categoryId = this.getCategoryId(this.state.category);
+	    let imageFile = this.state.productImg;
+	    console.log(imageFile);
 
-      if(valid == 1){	
 	    formData.append('add_product', 'adding product');
         formData.append('user_id', this.state.user.value.id);
 	    formData.append('product_name', this.state.productName);
@@ -98,24 +113,53 @@ class AddProduct extends React.Component {
 	    formData.append('product_price', this.state.price);
 	    formData.append('category_id', categoryId);
 	    formData.append('product_image', imageFile);
-		for (var key of formData.entries()) {
+	    for (var key of formData.entries()) {
         console.log(key[0] + ', ' + key[1]);
+        }
+          axios({
+            method: 'POST',
+            url:'https://i383988.hera.fhict.nl/database.php?',
+            data: formData,
+            config: {headers:{'Content-Type': 'multipart/form-data'}}
+          }).then(function(response){
+            alert("Your product has been listed");
+          })
+	  }     
     }
-        axios({
-          method: 'POST',
-          url:'https://i383988.hera.fhict.nl/database.php?',
-          data: formData,
-          config: {headers:{'Content-Type': 'multipart/form-data'}}
-        }).then(function(response){
-          console.log(response);
-          console.log('Product added');
-          alert("Your product has been listed");
-        })
-      }
-      else{
-        alert("Please enter valid data!");
-      }
-    }
+	
+	validateForm(){
+		let alertString = "";
+		let validForm = true;
+		if(this.state.user.value == null){
+			alert("Not signed into an account!");
+			validForm = false;
+			return validForm;
+		}
+		if(this.state.productName == null || !(/\S/.test(this.state.productName))){
+			alertString += "The product name is empty.\n";
+			validForm = false;
+		}
+		if(this.state.productDesc == null || !(/\S/.test(this.state.productDesc))){
+			alertString += "The product description is empty.\n";
+			validForm = false;
+		}
+		if(Number.isNaN(this.state.parsedPrice) || !(/\S/.test(this.state.parsedPrice)) || this.state.parsedPrice <= 0 || this.state.parsedPrice == "NaN"){
+			alertString += "The price is not a valid positive number.\n";
+			validForm = false;
+		}
+		if(this.getCategoryId(this.state.category) == -1){
+			alertString += "No category has been selected for your product.\n";
+			validForm = false;
+		}
+		if(this.state.productImg == null){
+			alertString += "No image has been added for your product.\n";
+			validForm = false;
+		}
+		if(!validForm){
+			alert(alertString);
+		}
+		return validForm;
+	}
 
   render(){	
   
@@ -191,6 +235,7 @@ class AddProduct extends React.Component {
                     <FormControlLabel value="book" control={<Radio />} label="Book" />
                     <FormControlLabel value="accessory" control={<Radio />} label="Accessory" />
                     <FormControlLabel value="bicycle" control={<Radio />} label="Bicycle" />
+					<FormControlLabel value="other" control={<Radio />} label="Other" />
                   </RadioGroup>
               </FormControl>
 
